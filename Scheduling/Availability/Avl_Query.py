985744c26ca4ba51fa6877bf9330bb86c3167206
Will Hopper <whopper@cat.pdx.edu>
2012-04-11 01:07:51 -0700

    Rework the database tables for ease of reading. Scripts updated to match new framework.

--- Scheduling/Availability/Avl_Query.py
+++ Scheduling/Availability/Avl_Query.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
 import sys, string, MySQLdb
 
 def Display_All_Avail(db_cursor):
-  db_cursor.execute("SELECT * FROM deskcat_availability ORDER BY FIELD(day, 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'), hour")
+  db_cursor.execute("SELECT * FROM deskcat_avl2 ORDER BY FIELD(day, 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT')")
   data = db_cursor.fetchall()
   Display_Header()
   Generate_Output(data)
 
 
-def Display_Day_Avail(day, db_cursor):
-  db_cursor.execute("SELECT * FROM deskcat_availability WHERE day = '%s' ORDER BY hour" % day)
+def Display_Day_Avail(sub_selection, db_cursor):
+  query_day = sub_selection[0:3]
+  db_cursor.execute("SELECT * FROM deskcat_avl2 WHERE day = '%s'" % query_day)
   data = db_cursor.fetchall()
   Display_Header()
   Generate_Output(data)
 
-def Display_DeskcatAvail_ByDay(deskcat, db_cursor):
-  db_cursor.execute("SELECT * FROM deskcat_availability WHERE deskcat = '%s' ORDER BY FIELD(day, 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'), hour" % deskcat)
+def Display_DeskcatAvail_ByDay(sub_selection, db_cursor):
+  db_cursor.execute("SELECT * FROM deskcat_avl2 WHERE deskcat = '%s' ORDER BY FIELD(day, 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT')" % sub_selection)
   data = db_cursor.fetchall()
   Display_Header()
   Generate_Output(data)
 
 def  No_Recent_Update(db_cursor):
-  print "  +===============+===============+"
-  print "  |   Deskcat     |   Last Update |"
-  print "  +===============+===============+"
-  db_cursor.execute("select distinct deskcat, last_updated from deskcat_availability where last_updated < date_sub(now(), interval 1 month)")
+  print "+===========+===============+"
+  print "|   Deskcat |   Last Update |"
+  print "+===========+===============+"
+  db_cursor.execute("SELECT DISTINCT deskcat, updated FROM deskcat_avl2 WHERE updated < date_sub(now(), interval 1 month)")
   data = db_cursor.fetchall()
   Generate_Output(data)
-
-def Display_Insufficient_Hours(db_cursor):
-  print "+=============+===========+"
-  print "|  Num Hours |   Deskcat  |"
-  print "+=============+===========+"
-  db_cursor.execute("SELECT COUNT(*), deskcat FROM deskcat_availability GROUP BY deskcat HAVING COUNT(*) < 8")
-  data = db_cursor.fetchall()
-  Generate_Output(data)
-
 
 
 #====================================================================================+
@@ -51,7 +44,7 @@ def Generate_Output(data):
     y = 0
     result_string = ""
     while y < len(data[i]):
-      result_string += ("   |  %s   " % str(data[i][y]))
+      result_string += ("|   %s " % str(data[i][y]))
       y = y + 1
     print result_string
     i = i + 1
@@ -59,7 +52,8 @@ def Generate_Output(data):
 
 def Display_Header():
   print "\n"
-  print "+====================+============+==================+===========+=========+"
-  print "|      Day           |    Hour    |    Last Updated  | Preferred | Deskcat |"
-  print "+====================+============+==================+===========+=========+\n"
+  print " _______ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ ______________ _________ "
+  print "| Day   |08:00|09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00| Last Updated | Deskcat |"
+  print " ‾‾‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾  ‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾ "
+
 
--- Scheduling/admin-core.py
+++ Scheduling/admin-core.py
@@ -12,35 +12,36 @@ def main ():
                         passwd="",db="availability")
   db_cursor = db.cursor()
 
-  while(int(selection) != 6):
+  while(int(selection) != 5):
+    sub_selection = ""
     # To be replaced with fancy curses interface, says monleezy
     print("\n\n+======================================================+")
     print("|               Welcome, Herder of CATS                |")
     print("|  1) Display All Availability in Weekly View          |")
     print("|  2) Display All Availability for a Specific Day      |")
     print("|  3) Display All Availability for a Specific DeskCAT  |")
-    print("|  4) Show DeskCATS with < 8 Hours of Availability     |")
-    print("|  5) Show DeskCATS Who Haven't Updated Availability   |")
-    print("|  6) Exit                                             |")
+    print("|  4) Show DeskCATS Who Haven't Updated Availability   |")
+    print("|  5) Exit                                             |")
     print("+======================================================+\n")
 
     selection = raw_input("Select an Action: ")
 
-    if(int(selection) == 1):
+    if int(selection) == 1:
       Avl_Query.Display_All_Avail(db_cursor)
 
-    elif(int(selection) == 2):
-      day = raw_input("View all availability for which day?\n")
-      Avl_Query.Display_Day_Avail(day, db_cursor)
+    elif int(selection) == 2:
+      while sub_selection != "back":
+        sub_selection = raw_input("\nView all availability for which day? (\"back\" to return to menu)\n")
+        if sub_selection != "back":
+          Avl_Query.Display_Day_Avail(sub_selection, db_cursor)
 
-    elif(int(selection) == 3):
-      deskcat = raw_input("View all availability for which Deskcat?\n")
-      Avl_Query.Display_DeskcatAvail_ByDay(deskcat, db_cursor)
+    elif int(selection) == 3:
+      while sub_selection != "back":
+        sub_selection = raw_input("\nView all availability for which Deskcat? (\"back\" to return to menu)\n")
+        if sub_selection != "back":
+          Avl_Query.Display_DeskcatAvail_ByDay(sub_selection, db_cursor)
 
-    elif(int(selection) == 4):
-      Avl_Query.Display_Insufficient_Hours(db_cursor)
-
-    elif(int(selection) == 5):
+    elif int(selection) == 4:
       Avl_Query.No_Recent_Update(db_cursor)
 
 
